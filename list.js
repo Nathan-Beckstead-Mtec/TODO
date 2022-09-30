797e1ce17bb46fa0d7a997c454ade3a1629c4b5d
Nathan-Beckstead-Mtec <Nathan.beckstead6282@stu.Mtec.edu>
2022-09-30 12:07:39 -0600

    nav-list input box can now rename the list. Done for today

--- list.js
+++ list.js
@@ -13,47 +13,47 @@ load_event_listener();
 /*
 
 [{
-        name: "TODO: todo list app",
-        items: [
-            {
-                text: "rename items",
-                checked: false
-            },
-            {
-                text: "prerequisites? (ie require a previous item to be compleated before this one appears)",
-                checked: false
-            },
-            {
-                text: "change color of checked off items",
-                checked: true
-            }
-        ]
-    },
-    {
-        name: "test",
-        items: []
-    },
-    {
-        name: "empty list",
-        items: [
-            {
-                text: "a",
-                checked: false
-            },
-            {
-                text: "c",
-                checked: true
-            },
-            {
-                text: "d",
-                checked: true
-            },
-            {
-                text: "e",
-                checked: false
-            }
-        ]
-    }
+		name: "TODO: todo list app",
+		items: [
+			{
+				text: "rename items",
+				checked: false
+			},
+			{
+				text: "prerequisites? (ie require a previous item to be compleated before this one appears)",
+				checked: false
+			},
+			{
+				text: "change color of checked off items",
+				checked: true
+			}
+		]
+	},
+	{
+		name: "test",
+		items: []
+	},
+	{
+		name: "empty list",
+		items: [
+			{
+				text: "a",
+				checked: false
+			},
+			{
+				text: "c",
+				checked: true
+			},
+			{
+				text: "d",
+				checked: true
+			},
+			{
+				text: "e",
+				checked: false
+			}
+		]
+	}
 ]
 */
 
@@ -62,11 +62,11 @@ load_event_listener();
 
 
 
-function generate_item_Html(item, index){
+function generate_item_Html(item, index) {
 	//should not be used directly because of the link between item and idex
 	const trash = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="4" y1="7" x2="20" y2="7"></line><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path></svg>`;
 	const sublist = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-subtask" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="6" y1="9" x2="12" y2="9"></line><line x1="4" y1="5" x2="8" y2="5"></line><path d="M6 5v11a1 1 0 0 0 1 1h5"></path><rect x="12" y="7" width="8" height="4" rx="1"></rect><rect x="12" y="15" width="8" height="4" rx="1"></rect></svg>`;
-	
+
 	return `
 	<div class="card${item.checked ? ' checked' : ''}" >
 	<input type="checkbox" id="li_${index}" ${item.checked ? 'checked="checked"' : ''} oninput="check_checker()">
@@ -83,16 +83,16 @@ function generate_item_Html(item, index){
 	// return `<label class="card" for="li_${index}"><input type="checkbox" id="li_${index}" ${item.checked ? 'checked="checked"' : ''} oninput="check_checker()"><p>${item.text}</p></label>`;
 }
 // unused but works
-function generate_item_Html_from_index(index){
-	return generate_item_Html(lists[index_list_selected].items[index],index);
+function generate_item_Html_from_index(index) {
+	return generate_item_Html(lists[index_list_selected].items[index], index);
 }
 // unused but works
-function generate_item_Html_last(){
-	return generate_item_Html_from_index(lists[index_list_selected].items.length -1);
+function generate_item_Html_last() {
+	return generate_item_Html_from_index(lists[index_list_selected].items.length - 1);
 }
 
 
-function render_items(){
+function render_items() {
 	const trashIconSvg = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
 	<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
 	<line x1="4" y1="7" x2="20" y2="7"></line>
@@ -101,32 +101,34 @@ function render_items(){
 	<path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
 	<path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
  </svg>`;
-    let listHtml = "";
-    if(lists.length === 0){
+	let listHtml = "";
+	if (lists.length === 0) {
 		listHtml = `<div class="empty"><p>&lt;----- create a new list to start</p></div>`;
 		document.querySelector("#add_item").disabled = true;
-	} else{
-		lists[index_list_selected].items.forEach((item,index) => {
+	} else {
+		lists[index_list_selected].items.forEach((item, index) => {
 			listHtml += generate_item_Html(item, index);
-    	})
+		})
 		document.querySelector("#add_item").disabled = false;
 	}
 	// listHtml += `<div class="edit"><input placeholder="+ add new item" id="add_item"></input></div>`
-
+	let list_name = document.querySelector("#list-name");
+	list_name.setAttribute("placeholder", lists[index_list_selected].name + ":");
+	list_name.value = "";
 
 	document.querySelector("#list").innerHTML = listHtml;
 
 }
 
-function render_lists(){
+function render_lists() {
 	let list_o_listsHtml = "";
-	lists.forEach((item,index) => {
-        list_o_listsHtml += `
+	lists.forEach((item, index) => {
+		list_o_listsHtml += `
 		<input id="lists_${index}" onclick="changed_selected_list(${index});" ${index == index_list_selected ? 'checked="checked"' : ''}type="radio" name="selected-list" style="display:none;">
 		<label for="lists_${index}">
 			<div><p class="list-name">${item.name}</p></div>
 		</label>`
-    });
+	});
 
 	list_o_listsHtml += `
 	<a onclick="new_list();">
@@ -140,26 +142,26 @@ function render_lists(){
 }
 
 
-function changed_selected_list(new_index){
+function changed_selected_list(new_index) {
 	index_list_selected = new_index;
 	render_items();
 	save();
 }
 
-function new_list(){
+function new_list() {
 	let name = prompt("Name of new List:", "unnamed list");
-	if (name == null){
+	if (name == null) {
 		return;
 	}
 	name = sanitize(name);
-	lists.push({name: name,items: []})
+	lists.push({ name: name, items: [] })
 	index_list_selected = lists.length - 1;
 	render_lists();
 	render_items();
 	save();
 }
 
-function sanitize(input){
+function sanitize(input) {
 	console.error("TODO: implement HTML sanitizer");
 	return input;
 }
@@ -167,34 +169,34 @@ function sanitize(input){
 
 
 
-function textbox_update(event){
-    if (event.key === "Enter"){
-        event.preventDefault(); //dont let the enter key scroll the page
+function textbox_update(event) {
+	if (event.key === "Enter") {
+		event.preventDefault(); //dont let the enter key scroll the page
 		let input = document.querySelector("#add_item"); //text input
-		
-		
-		let item_to_push = {text: input.value, checked:false};
+
+
+		let item_to_push = { text: input.value, checked: false };
 		let index = lists[index_list_selected].items.length; //off by one accounted for below
 		lists[index_list_selected].items.push(item_to_push); //add the thing
-		
+
 		input.value = ""; //clears the textbox again
 
 
 		render_items();
 		save();
-    }
+	}
 }
 
 
-function unload_event_listener(){ //unused
+function unload_event_listener() { //unused
 	document.querySelector("#add_item").removeEventListener("keypress", textbox_update);
 }
-function load_event_listener(){ //used once (move to start of prog?)
+function load_event_listener() { //used once (move to start of prog?)
 	document.querySelector("#add_item").addEventListener("keypress", textbox_update);
 }
 
 
-function check_checker(){
+function check_checker() {
 	// let num_changed = 0;
 	// let num_elem = 0;
 	// console.clear();
@@ -206,15 +208,15 @@ function check_checker(){
 		let bool = elem.checked;
 		let id = parseInt(elem.id.slice(3));
 
-		if(isNaN(id)){
+		if (isNaN(id)) {
 			console.error("check_checker(): Couldn't parse id from " + elem.id);
 			return;
 		}
 
-		elem.parentElement.classList.toggle("checked",bool);
+		elem.parentElement.classList.toggle("checked", bool);
 		// old = lists[index_list_selected].items[id].checked;
 		lists[index_list_selected].items[id].checked = bool;
-		
+
 		// if (bool != old){
 		// 	console.log("changed " + id + " from " + old + " to " + bool);
 		// 	num_changed++;
@@ -234,52 +236,58 @@ function save() {
 	console.log("saved");
 }
 
-function load(){
+function load() {
 	let temp_lists = localStorage.getItem("lists");
 	let temp_index = localStorage.getItem("currentList");
-	if (temp_lists == null){
+	if (temp_lists == null) {
 		lists = [];
 		console.log("couldn't accsess or find saved list");
-	} else{
+	} else {
 		lists = JSON.parse(temp_lists);
 	}
-	if (temp_index == null){
+	if (temp_index == null) {
 		index_list_selected = 0;
 		console.log("couldn't accsess or find saved list");
-	} else{
+	} else {
 		index_list_selected = JSON.parse(temp_index);
 	}
 }
 
 
-function card_button_sublist(id){
+function card_button_sublist(id) {
 
 }
-function card_button_trash(index){
+function card_button_trash(index) {
 	moveToTrash({
-		from:lists[index_list_selected].name,
-		item:lists[index_list_selected].items[index]});
+		from: lists[index_list_selected].name,
+		item: lists[index_list_selected].items[index]
+	});
 
-	lists[index_list_selected].items.splice(index,1);
+	lists[index_list_selected].items.splice(index, 1);
 	save();
 	render_items();
 }
 
 
-function moveToTrash(input){
+function moveToTrash(input) {
 	console.log("deleated:");
 	console.log(input)
 }
 
 
-function sort_clicked(){
+
+
+
+//list-nav
+
+function sort_clicked() {
 	console.log("sort");
-	lists[index_list_selected].items.sort((a,b) =>{
+	lists[index_list_selected].items.sort((a, b) => {
 		console.log(JSON.stringify(a) + ", " + JSON.stringify(b));
-		if (a.checked == b.checked){
+		if (a.checked == b.checked) {
 			return 0;
 		}
-		if(a.checked){
+		if (a.checked) {
 			return 1;
 		}
 		//a != b and a == false \implies b == true
@@ -290,44 +298,55 @@ function sort_clicked(){
 }
 
 
-function delete_selected(){
+function delete_selected() {
 	console.log("delete selected");
 	check_checker();
-	lists[index_list_selected].items = lists[index_list_selected].items.filter((item, index) =>{
-		if(!item.checked){
+	lists[index_list_selected].items = lists[index_list_selected].items.filter((item, index) => {
+		if (!item.checked) {
 			return true; //keep element
 		}
 		moveToTrash({
-			from:lists[index_list_selected].name,
-			item:item
+			from: lists[index_list_selected].name,
+			item: item
 		});
 		return false; //toss the element
 	});
 	save();
 	render_items();
 }
-function delete_list(){
+function delete_list() {
 	console.log("delete all");
 	//confirm
-	if(!confirm("Delete list: " + lists[index_list_selected].name + "?")){
+	if (!confirm("Delete list: " + lists[index_list_selected].name + "?")) {
 		return;
 	}
 	//send  to trash as sublist
 	// moveToTrash()
 
 	//delete list
-	lists.splice(index_list_selected,1);
+	lists.splice(index_list_selected, 1);
 
 	//ui
 	// animation?
 
 	//send to different list
-	
+
 	index_list_selected--; //underflow?
 	//do something if index == 0 (before dec) then 
-		// either go to next list
-		// or generate the null list.
+	// either go to next list
+	// or generate the null list.
 	render_lists();
-	save(); 
+	save();
 
 }
+
+function nav_list_rename(thus){
+	let value = thus.value;
+	lists[index_list_selected].name = value;
+	document.querySelector(`label[for="lists_${index_list_selected}"] p`).textContent = value;
+	render_items();
+	save();
+
+
+}
+
